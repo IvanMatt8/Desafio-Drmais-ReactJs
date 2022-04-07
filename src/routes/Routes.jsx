17460a28cead9bb021d9c07dcccc9aeb499e9602
IvanMatt8 <ivanmatt1995@gmail.com>
2022-04-07 20:23:42 -0300

    :sparkles: adicionado novas rotas, cadastro e editar.

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,11 +1,16 @@
 import React from "react"
 import { Routes, Route } from "react-router-dom"
 import Home from "../pages/home"
+import Cadastros from "../pages/cadastros/index"
+import EditCurso from "../pages/editCurso/index"
 
 function Rotas() {
   return (
     <Routes>
       <Route exact path="/" element={<Home />} />
+      <Route path="cadastros" element={<Cadastros />} />
+      <Route path="editar" element={<EditCurso />} />
+      <Route path="*" element={<Home />} />
     </Routes>
   )
 }
