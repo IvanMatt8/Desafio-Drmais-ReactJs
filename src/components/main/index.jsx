4509a9950f3d5e9a2e9479621207c2b0fe0291af
IvanMatt8 <ivanmatt1995@gmail.com>
2022-04-07 20:28:03 -0300

    :sparkles: import do header no Main.

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,9 +1,11 @@
 import "./index.css"
 import React from "react"
+import Header from "../header/index"
 
 function Main(props) {
   return (
     <React.Fragment>
+      <Header {...props} />
       <main className="content container-fluid">
         <div className="p-3 mt-3">{props.children}</div>
       </main>
