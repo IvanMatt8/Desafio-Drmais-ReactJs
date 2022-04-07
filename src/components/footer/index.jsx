a45a711eccd3977f588b2a216de14b324bb02ec9
IvanMatt8 <ivanmatt1995@gmail.com>
2022-04-07 20:24:54 -0300

    :sparkles: adicionado novo componente Footer.

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,17 @@
+import "./index.css"
+import React from "react"
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <span>
+        Desenvolvido por
+        <strong>
+          Ivan <span className="text-danger">M</span>att
+        </strong>
+      </span>
+    </footer>
+  )
+}
+
+export default Footer
