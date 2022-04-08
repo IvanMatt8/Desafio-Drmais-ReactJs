9d470788a4dee77c9f9ddb91d1357277992d3831
IvanMatt8 <ivanmatt1995@gmail.com>
2022-04-08 19:09:50 -0300

    :sparkles: adicionado logica e import do useContext no cadastros.

--- src/pages/cadastros/index.jsx
+++ src/pages/cadastros/index.jsx
@@ -1,26 +1,85 @@
 import React from "react"
 import Input from "../../components/input/index"
+import { Formik } from "formik"
+import { useCursoContent } from "../../hooks/context"
 
 export default function Registros() {
+  const { handleAddCurso } = useCursoContent()
+
   return (
     <div className="container-md p-5">
       <div className="d-flex justify-content-center w-100">
         <h2>Novo Curso</h2>
       </div>
-      <div className="col-12 d-flex justify-content-end ">
-        <button className="btn btn-success" onClick={(e) => this.save(e)}>
-          Salvar
-        </button>
-      </div>
+      <div className="col-12 d-flex justify-content-end "></div>
 
       <hr />
+
       <div className="d-flex align-self-center justify-content-center ">
         <div className="card d-flex align-self-center  w-100">
-          <div className="card-body p-3 .d-flex .flex-direction-column">
-            <Input className="col-4 " label="Código" type="text" placeholder="Digite o Código" />
-            <Input className="col-12  mt-3" label="Título" type="text" placeholder="Digite o Título do Curso" />
-            <Input className="col-12 mt-3 " label="Descrição" as="textarea" type="text" placeholder="Descreva sobre o Curso" />
-          </div>
+          <Formik
+            initialValues={{ codigo: "", titulo: "", descricao: "" }}
+            validate={(values) => {
+              const errors = {}
+              if (!values.codigo) {
+                errors.codigo = "Required"
+              }
+
+              if (!values.titulo) {
+                errors.titulo = "Required"
+              }
+
+              if (!values.descricao) {
+                errors.descricao = "Required"
+              }
+
+              return errors
+            }}
+            onSubmit={(values, { setSubmitting }) => {
+              handleAddCurso({ ...values })
+              setSubmitting(false)
+            }}
+          >
+            {({ values, errors, handleChange, handleSubmit, isSubmitting }) => (
+              <form className="card-body p-3 .d-flex .flex-direction-column" onSubmit={handleSubmit}>
+                <Input
+                  name="codigo"
+                  onChange={handleChange}
+                  value={values.codigo}
+                  className="col-4 "
+                  label="Código"
+                  type="text"
+                  placeholder="Digite o Código"
+                />
+                {errors.codigo && <p className="text-danger  ml-3 mt-2">Codigo e requirido</p>}
+                <Input
+                  name="titulo"
+                  onChange={handleChange}
+                  value={values.titulo}
+                  className="col-12  mt-3"
+                  label="Título"
+                  type="text"
+                  placeholder="Digite o Título do Curso"
+                />
+                {errors.titulo && <p className="text-danger  ml-3 mt-2">Titulo e requirido</p>}
+                <Input
+                  name="descricao"
+                  onChange={handleChange}
+                  value={values.descricao}
+                  className="col-12 mt-3 "
+                  label="Descrição"
+                  as="textarea"
+                  type="text"
+                  placeholder="Descreva sobre o Curso"
+                />
+                {errors.descricao && <p className="text-danger  ml-3 mt-2">Descrição e requirido</p>}
+
+                <button disabled={isSubmitting} type="submit" className="btn btn-success ml-3 mt-5">
+                  Salvar
+                </button>
+              </form>
+            )}
+          </Formik>
         </div>
       </div>
     </div>
